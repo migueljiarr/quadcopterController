8688233910cbed0003cafdbeb049a89cf629dc2d
Miguel Jimenez <miguelavmja@gmail.com>
2016-11-09 13:47:27 +0100

    Now we apply force to where the rotors should be.

--- Assets/Scripts/HeightStabiliser.cs
+++ Assets/Scripts/HeightStabiliser.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class HeightStabiliser : MonoBehaviour {
 
-    public Vector3 applyPositionZ;
-    public Vector3 applyPositionX;
     public Vector3 setPoint;
     public float pGain;
     public float iGain;
     public float dGain;
+
+    public GameObject zCWRotor;
+    public GameObject zCCWRotor;
+    public GameObject xCWRotor;
+    public GameObject xCCWRotor;
+    Vector3 applyPositionPZ;
+    Vector3 applyPositionNZ;
+    Vector3 applyPositionPX;
+    Vector3 applyPositionNX;
+
     Vector3 force;
     Vector3 curPos;
     Rigidbody rb;
@@ -68,8 +76,12 @@ public class HeightStabiliser : MonoBehaviour {
     // Use this for initialization
     void Start () {
         /* For those with time constraints: */
-        //Time.timeScale=5f;
-        rb = gameObject.transform.root.gameObject.GetComponent<Rigidbody>();
+        Time.timeScale=1f;
+        rb = gameObject.GetComponent<Rigidbody>();
+        applyPositionPZ = zCWRotor.transform.position;
+        applyPositionNZ = zCCWRotor.transform.position;
+        applyPositionPX = xCWRotor.transform.position;
+        applyPositionNZ = xCCWRotor.transform.position;
         transform.position = new Vector3(0f,0.5f,0f);
         force = new Vector3(0f,9.81f/4,0f);
 
@@ -81,11 +93,22 @@ public class HeightStabiliser : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate () {
         curPos = transform.position;
+        /* Local position meaning relative to Quadcopter origin.*/
+        applyPositionPZ = zCWRotor.transform.position;
+        applyPositionNZ = zCCWRotor.transform.position;
+        applyPositionPX = xCWRotor.transform.position;
+        applyPositionNX = xCCWRotor.transform.position;
         Vector3 f = controller.updateOutputSignal(curPos);
         Debug.Log("F: " + f);
-        rb.AddForceAtPosition(f, curPos+applyPositionZ, ForceMode.Force); 
-        rb.AddForceAtPosition(f, curPos-applyPositionZ, ForceMode.Force); 
-        rb.AddForceAtPosition(f, curPos+applyPositionX, ForceMode.Force); 
-        rb.AddForceAtPosition(f, curPos-applyPositionX, ForceMode.Force); 
+        //Vector3 force = new Vector3(0f,9.81f/4,0f);
+        //Debug.Log("force: " + force);
+        Debug.Log("applyPositionPZ: " + applyPositionPZ);
+        Debug.Log("applyPositionNZ: " + applyPositionNZ);
+        Debug.Log("applyPositionPX: " + applyPositionPX);
+        Debug.Log("applyPositionNX: " + applyPositionNX);
+        rb.AddForceAtPosition(f, curPos+applyPositionPZ, ForceMode.Force); 
+        rb.AddForceAtPosition(f, curPos+applyPositionNZ, ForceMode.Force); 
+        rb.AddForceAtPosition(f, curPos+applyPositionPX, ForceMode.Force); 
+        rb.AddForceAtPosition(f, curPos+applyPositionNX, ForceMode.Force); 
     }
 }
