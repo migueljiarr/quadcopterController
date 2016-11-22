ca67144a5ccbead3add1288907f29a3e17e8ebf7
Miguel Jimenez <miguelavmja@gmail.com>
2016-11-22 21:49:38 +0100

    Refactorized to make actuator-controller differentation. Due to 4 more updates, this may be causing performance issues.

--- Assets/Scripts/HeightStabiliser.cs
+++ Assets/Scripts/HeightStabiliser.cs
@@ -12,14 +12,9 @@ public class HeightStabiliser : MonoBehaviour {
     public GameObject zCCWRotor;
     public GameObject xCWRotor;
     public GameObject xCCWRotor;
-    Vector3 applyPositionPZ;
-    Vector3 applyPositionNZ;
-    Vector3 applyPositionPX;
-    Vector3 applyPositionNX;
 
     Vector3 force;
     Vector3 curPos;
-    Rigidbody rb;
 
     PIDController controller;
 
@@ -27,38 +22,40 @@ public class HeightStabiliser : MonoBehaviour {
     void Start () {
         /* For those with time constraints: */
         Time.timeScale=1f;
-        rb = gameObject.GetComponent<Rigidbody>();
-        applyPositionPZ = zCWRotor.transform.position;
-        applyPositionNZ = zCCWRotor.transform.position;
-        applyPositionPX = xCWRotor.transform.position;
-        applyPositionNZ = xCCWRotor.transform.position;
         transform.position = new Vector3(0f,0.5f,0f);
         force = new Vector3(0f,9.81f/4,0f);
 
         controller = new PIDController(setPoint,pGain,iGain,dGain,force,transform.position);
-        //controller = new PIDController(setPoint,gain,new Vector3(0f,0f,0f));
         controller.updateOutputSignal(transform.position);
     }
     
     // Update is called once per frame
     void FixedUpdate () {
         curPos = transform.position;
-        /* Local position meaning relative to Quadcopter origin.*/
-        applyPositionPZ = zCWRotor.transform.position;
-        applyPositionNZ = zCCWRotor.transform.position;
-        applyPositionPX = xCWRotor.transform.position;
-        applyPositionNX = xCCWRotor.transform.position;
         Vector3 f = controller.updateOutputSignal(curPos);
-        Debug.Log("F: " + f);
-        //Vector3 force = new Vector3(0f,9.81f/4,0f);
-        //Debug.Log("force: " + force);
-        Debug.Log("applyPositionPZ: " + applyPositionPZ);
-        Debug.Log("applyPositionNZ: " + applyPositionNZ);
-        Debug.Log("applyPositionPX: " + applyPositionPX);
-        Debug.Log("applyPositionNX: " + applyPositionNX);
-        rb.AddForceAtPosition(f, curPos+applyPositionPZ, ForceMode.Force); 
-        rb.AddForceAtPosition(f, curPos+applyPositionNZ, ForceMode.Force); 
-        rb.AddForceAtPosition(f, curPos+applyPositionPX, ForceMode.Force); 
-        rb.AddForceAtPosition(f, curPos+applyPositionNX, ForceMode.Force); 
+        if(f.y < 0){
+            zCCWRotor.GetComponent<Rotor>().setForce(f);
+        }
+        else{
+            zCWRotor.GetComponent<Rotor>().setForce(f);
+        }
+        if(f.y < 0){
+            zCWRotor.GetComponent<Rotor>().setForce(f);
+        }
+        else{
+            zCCWRotor.GetComponent<Rotor>().setForce(f);
+        }
+        if(f.y < 0){
+            xCCWRotor.GetComponent<Rotor>().setForce(f);
+        }
+        else{
+            xCWRotor.GetComponent<Rotor>().setForce(f);
+        }
+        if(f.y < 0){
+            xCWRotor.GetComponent<Rotor>().setForce(f);
+        }
+        else{
+            xCCWRotor.GetComponent<Rotor>().setForce(f);
+        }
     }
 }
---
+++ Assets/Scripts/Rotor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotor : MonoBehaviour {
+
+    public Vector3 force;
+    Vector3 curPos;
+    Rigidbody rb;
+
+    public void setForce(Vector3 f){
+        force = f;
+    }
+
+    void Start(){
+        rb = gameObject.transform.root.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        curPos = transform.position;
+        Debug.Log("Force: " + force + " at position: " + curPos + " aka: " + gameObject.name);
+        rb.AddForceAtPosition(force, curPos, ForceMode.Force); 
+    }
+}
